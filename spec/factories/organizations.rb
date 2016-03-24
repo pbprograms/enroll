426044a64702731a2db8a3e362660d0c2bb9a7a5
Battula <vrbattula@outlook.com>
2016-03-24 13:10:24 -0400

    Refs 6112 Broker Quote - Roster creation

--- spec/factories/organizations.rb
+++ spec/factories/organizations.rb
@@ -45,4 +45,16 @@ FactoryGirl.define do
       organization.employer_profile = FactoryGirl.create :employer_profile, organization: organization
     end
   end
+  factory :broker, class: Organization do
+    sequence(:legal_name) {|n| "Broker Agency#{n}" }
+    sequence(:dba) {|n| "Broker Agency#{n}" }
+    sequence(:fein, 200000000)
+    home_page   "http://www.example.com"
+    office_locations  { [FactoryGirl.build(:office_location, :primary),
+                         FactoryGirl.build(:office_location)] }
+    
+    before :create do |organization, evaluator|
+      organization.broker_agency_profile = FactoryGirl.build :broker_agency_profile, organization: organization
+    end
+  end
 end
