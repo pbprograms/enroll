8c63ddcfc32bf556b396f81324cadc9bd8f8770c
lisyk <sergey.liskovoy@gmail.com>
2016-04-18 17:28:47 -0400

    Bug #5417 #5419 remove datatable, add simple pagination

--- app/controllers/exchanges/hbx_profiles_controller.rb
+++ app/controllers/exchanges/hbx_profiles_controller.rb
@@ -135,7 +135,7 @@ class Exchanges::HbxProfilesController < ApplicationController
   end
 
   def verification_index
-    @families = Family.by_enrollment_individual_market.where(:'households.hbx_enrollments.aasm_state' => "enrolled_contingent")
+    @families = Family.by_enrollment_individual_market.where(:'households.hbx_enrollments.aasm_state' => "enrolled_contingent").page(params[:page]).per(3)
     respond_to do |format|
       format.html { render partial: "index_verification" }
       format.js {}
